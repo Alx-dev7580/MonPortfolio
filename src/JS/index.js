bd2171222628795505c88cd46599f66f58d8aa70
Alx0088 <alex.painnot@gmail.com>
2021-06-22 13:19:34 +0200

    ajout projet1

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,137 +1,39 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("OK");
+const countries = document.querySelector(".countries"),
+sports = document.querySelector(".sports"),
+countriesSlider = document.querySelector(".main__slider--countries"),
+sportsSlider = document.querySelector(".main__slider--sports"),
+main = document.querySelector(".main");
 
-  class Model {
-    constructor() {
-      this.pages = [
-        {
-          title: "Homepage",
-          url: "#",
-          background: "black",
-          content: `
-						<div>
-							Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-							<ul>
-								<li>Lorem ipsum</li>
-								<li>dolor sit</li>
-								<li>amet consectetur</li>
-							</ul>
-						</div>
-					`,
-          dynamisme: () => {
-            document.querySelector("ul").querySelector("li").innerText =
-              "Ipsum Lorem";
-          },
-        },
-        {
-          title: "Contact",
-          url: "#contact",
-          background: "blue",
-          form: true,
-        },
-        {
-          title: "Test",
-          url: "#test",
-          background: "green",
-          color: "black",
-          content: `
-						<div>
-							Test
-							<ul>
-								<li>Lorem ipsum</li>
-								<li>dolor sit</li>
-								<li>amet consectetur</li>
-							</ul>
-						</div>
-					`,
-        },
-      ];
-    }
+countries.addEventListener("click", function() {
+countriesSlider.style.visibility = "visible";
+sportsSlider.style.visibility = "hidden";
+main.classList.remove("main--sports");
+this.classList.add("active");
+sports.classList.remove("active");
+});
 
-    getPageByUrl(url) {
-      return this.pages.find((page) => page.url == url);
+sports.addEventListener("click", function() {
+countriesSlider.style.visibility = "hidden";
+sportsSlider.style.visibility = "visible";
+main.classList.add("main--sports");
+this.classList.add("active");
+countries.classList.remove("active");
+});
+
+$(".main__slider").slick({
+centerMode: true,
+infinite: false,
+slidesToShow: 3,
+slidesToScroll: 1,
+arrows: false,
+initialSlide: 2,
+responsive: [
+  {
+    breakpoint: 1000,
+    settings: {
+      slidesToShow: 1,
+      fade: true
     }
   }
-
-  class View {
-    constructor(pages) {
-      this.container = document.querySelector(".container");
-      this.container.innerHTML = "";
-      this.addHeader(pages);
-    }
-
-    run(dynamisme) {
-      dynamisme();
-    }
-
-    addContent(content) {
-      let contentContainer = document.createElement("div");
-      contentContainer.classList.add("content");
-      contentContainer.innerHTML = content;
-
-      this.container.appendChild(contentContainer);
-    }
-
-    addHeader(pages) {
-      pages.forEach((page) => {
-        const pageButton = document.createElement("button");
-
-        pageButton.innerText = page.title;
-
-        this.container.appendChild(pageButton);
-
-        pageButton.addEventListener("click", () => {
-          location.hash = page.url;
-        });
-      });
-    }
-
-    changeTitle(text) {
-      const title = document.createElement("h1");
-
-      title.innerText = text;
-
-      this.container.appendChild(title);
-    }
-
-    changeBackground(color) {
-      this.container.style.background = color;
-    }
-
-    addForm() {
-      let input = document.createElement("input");
-      input.setAttribute("type", "text");
-      input.classList.add("input-text");
-
-      this.container.appendChild(input);
-    }
-  }
-
-  function controller() {
-    let data = new Model();
-
-    const currentPage = data.getPageByUrl(location.hash || "#");
-
-    let page = new View(data.pages);
-    page.changeTitle(currentPage.title);
-    page.changeBackground(currentPage.background);
-
-    if (currentPage.content) {
-      page.addContent(currentPage.content);
-    }
-
-    if (typeof currentPage.dynamisme === "function") {
-      page.run(currentPage.dynamisme);
-    }
-
-    if (currentPage.form) {
-      page.addForm();
-    }
-  }
-
-  window.addEventListener("hashchange", () => {
-    controller();
-  });
-
-  controller();
+]
 });
